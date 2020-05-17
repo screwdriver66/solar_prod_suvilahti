0fb08c5be0ce67cbfac3a68663db61f9036755dc
screwdriver66 <simo.uimonen@gmail.com>
2020-05-17 14:42:55 +0300

    release docker heroku11

--- packages/ml_api/run.sh
+++ packages/ml_api/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export IS_DEBUG=${DEBUG:-false}
-# export PORT=$PORT
-exec gunicorn --bind 0.0.0.0:5000 --access-logfile - --error-logfile - run:application
+export PORT=$PORT
+exec gunicorn --bind 0.0.0.0:$PORT --access-logfile - --error-logfile - run:application
 # exec gunicorn --bind 0.0.0.0:$PORT --access-logfile - --error-logfile - run:application
